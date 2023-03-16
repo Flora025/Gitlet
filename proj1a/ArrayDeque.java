f2ccf1631b61703b63f15467ae3ec3effcb36cef
Flora025 <2365349265@qq.com>
2023-03-17 01:14:06 +0800

    submit proj1a ?

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,138 +1,91 @@
 public class ArrayDeque<T> {
 
-    private int size;
     private int nextFirst;
     private int nextLast;
-    private T[] items;
-    private double R;
-
-
-    /** Creates an empty linked list deque. */
-    public ArrayDeque() {
-        size = 0;
-        items = (T[]) new Object[8];
-        nextFirst = items.length - 1;
-        nextLast = 0;
+    private int capacity;
+    private T[]items;
+    private int size;
+    public ArrayDeque(){
+        items=(T[])new Object[8];
+        this.capacity=items.length;
+        nextFirst=capacity-1;
+        nextLast=0;
+        size=0;
     }
-
-    /** moves one position front */
-    private int moveOneFront(int nextWhat) {
-        if (nextWhat == 0) {
-            return items.length - 1;
-        } else {
-            return nextWhat - 1;
-        }
+    private void resize(int capacity){
+        T[]a=(T[])new Object[capacity];
+        //由于nextFirst和nextLast的位置不确定，只能一个一个地复制到新的数组中
+        //从nextFirst右边的第一个点开始复制
+        //到nextLast左边的第一个点复制结束
+        for (int i=1;i<=size;i++)
+            a[i]=items[(++nextFirst)%this.capacity];
+        this.capacity=capacity;
+        //这两个指针指向什么地方已经不重要了
+        nextFirst=0;
+        nextLast=size+1;
+        items=a;
     }
-
-    /** moves one position back */
-    private int moveOneBack(int nextWhat) {
-        nextWhat %= items.length;
-        if (nextWhat == items.length - 1) {
-            return 0;
-        } else {
-            return nextWhat + 1;
-        }
-    }
-
-    /** Adds an item of type T to the front of the deque */
     public void addFirst(T item) {
-        if (size == items.length) {
-            resize(items.length * 2);
-        }
-
-        items[nextFirst] = item;
-        nextFirst = moveOneFront(nextFirst);
-        size += 1;
+        //直接当size等于capacity时调整大小，而不是看两个指针的相对位置
+        if (size==capacity)
+            resize(capacity*2);
+        items[nextFirst]=item;
+        size++;
+        //nextFirst有可能越界
+        nextFirst=nextFirst==0?capacity-1:nextFirst-1;
     }
 
-    /** Adds an item of type T to the back of the deque.
-     * must not use loop or recursion! */
     public void addLast(T item) {
-        if (size == items.length) {
-            resize(items.length * 2);
-        }
-
-        items[nextLast] = item;
-        nextLast = moveOneBack(nextLast);
-        size += 1;
+        if (size==capacity)
+            resize(capacity*2);
+        items[nextLast]=item;
+        size++;
+        //nextLast有可能越界
+        nextLast=(nextLast+1)%capacity;
     }
 
-    /** Returns true if deque is empty, false otherwise. */
     public boolean isEmpty() {
-        return size == 0;
+        return size==0;
     }
 
-    /** Returns the number of items in the deque. */
     public int size() {
         return size;
     }
 
-    /** Prints the items in the deque from first to last, separated by a space. */
     public void printDeque() {
-        for (T item: items) {
-            if (item != null) {
-                System.out.print(item);
-                System.out.print(' ');
-            }
-        }
+        //nextFirst有可能指向最后一个位置
+        for (int i=(nextFirst+1)%capacity;i!=nextLast-1;i=(i+1)%capacity)
+            System.out.print(items[i]+" ");
+        System.out.print(items[nextLast-1]);
     }
 
-    /** Removes and returns the item at the front of the deque.
-     * If no such item exists, returns null. */
     public T removeFirst() {
-        if (isEmpty()) {
-            return null;
-        }
-
-        nextFirst = moveOneBack(nextFirst);
-        T returnItem = items[nextFirst];
-        items[nextFirst] = null;
-        size -= 1;
-        if (items.length >= 16 && items.length / size > 4) {
-            resize(items.length / 2);
-        }
-        return returnItem;
+        //当数组的内容为空的时候，才无法进行remove操作，而不是取决于nextFirst的位置。
+        if (size==0)return null;
+        nextFirst=(nextFirst+1)%capacity;
+        T temp=items[nextFirst];
+        items[nextFirst]=null;
+        size--;
+        if (capacity>=16&&size<capacity/4)
+            resize(capacity/2);
+        return temp;
     }
 
-    /** Removes and returns the item at the back of the deque.
-     * If no such item exists, returns null.*/
     public T removeLast() {
-
-        if (isEmpty()) {
-            return null;
-        }
-
-        nextLast = moveOneFront(nextLast);
-        T returnItem = items[nextLast];
-        items[nextLast] = null;
-        size -= 1;
-        // save memp
-        if (items.length >= 16 && items.length / size > 4) {
-            resize(items.length / 2);
-        }
-        return returnItem;
+        if (size==0)return null;
+        nextLast=nextLast==0?capacity-1:nextLast-1;
+        T temp=items[nextLast];
+        items[nextLast]=null;
+        size--;
+        if (capacity>=16&&size<capacity/4)
+            resize(capacity/2);
+        return temp;
     }
 
-    /** Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth
-     * If no such item exists, returns null. Must not alter the deque!
-     * Must use iteration.
-     */
     public T get(int index) {
-        if (this.isEmpty()) {
+        if (index>=size)
             return null;
-        }
-        return items[(index + nextFirst + 1) % items.length];
-    }
-
-
-    /** resize the array (from the front / back) */
-    private void resize(int capacity) {
-        T[] des = (T[]) new Object[capacity];
-        for (int i = 1; i <= size; i += 1) {
-            des[i] = items[(nextFirst + 1) % capacity];
-        }
-        nextFirst = 0;
-        nextLast = size + 1;
-        items = des;
+        return items[(nextFirst+1+index)%capacity];
     }
 }
+
