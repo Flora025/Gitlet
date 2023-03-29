1972485d0e10105e989754333aeaee81a6bbeaed
Flora025 <2365349265@qq.com>
2023-03-29 23:51:14 +0800

    submit hw3

--- hw3/hw3/hash/OomageTestUtility.java
+++ hw3/hw3/hash/OomageTestUtility.java
@@ -1,5 +1,6 @@
 package hw3.hash;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class OomageTestUtility {
@@ -12,6 +13,23 @@ public class OomageTestUtility {
          * and ensure that no bucket has fewer than N / 50
          * Oomages and no bucket has more than N / 2.5 Oomages.
          */
-        return false;
+        // make a hashmap
+        HashMap<Integer, Integer> map = new HashMap<>(M);
+        for (Oomage o : oomages) {
+            int bucketNum = (o.hashCode() & 0x7FFFFFFF) % M;
+            if (map.containsKey(bucketNum)) {
+                map.put(bucketNum, 1 + map.get(bucketNum));
+            } else {
+                map.put(bucketNum, 1);
+            }
+        }
+        // no fewer than N / 50 and no more than N / 2.5
+        for (int val : map.values()) {
+            if (val < oomages.size() / 50
+                    || val > oomages.size() / 2.5) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- hw3/hw3/hash/TestComplexOomage.java
+++ hw3/hw3/hash/TestComplexOomage.java
@@ -36,15 +36,19 @@ public class TestComplexOomage {
     /* TODO: Create a list of Complex Oomages called deadlyList
      * that shows the flaw in the hashCode function.
      */
-    /*
     @Test
     public void testWithDeadlyParams() {
         List<Oomage> deadlyList = new ArrayList<>();
-
+        List<Integer> l = new ArrayList<>();
+        l.add(255);
+        l.add(255);
+        l.add(255);
+        ComplexOomage a = new ComplexOomage(l);
+        deadlyList.add(a);
         // Your code here.
 
         assertTrue(OomageTestUtility.haveNiceHashCodeSpread(deadlyList, 10));
-    } */
+    }
 
     /** Calls tests for SimpleOomage. */
     public static void main(String[] args) {
