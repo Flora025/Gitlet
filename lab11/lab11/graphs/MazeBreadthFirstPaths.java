fbea14f2fa69a82b8dbeab890d56eb8dbac8a316
Flora025 <2365349265@qq.com>
2023-04-04 18:57:10 +0800

    submit lab11

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,5 +1,7 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.Queue;
+
 /**
  *  @author Josh Hug
  */
@@ -9,21 +11,51 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private Maze maze;
+    private int s; // start 1-D coordinate
+    private int t; // target 1-D coordinate
+    private boolean targetFound = false;
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
         // Add more variables here!
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
     private void bfs() {
-        // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        // Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        Queue<Integer> queue = new Queue<>();
+        queue.enqueue(s);
+        marked[s] = true;
+
+        while (!queue.isEmpty()) {
+            int prev = queue.dequeue();
+            if (prev == t) {
+                targetFound = true;
+                return;
+            }
+            announce();
+            for (int next : maze.adj(prev)) {
+                if (!marked[next]) {
+                    queue.enqueue(next);
+                    marked[next] = true;
+                    edgeTo[next] = prev;
+                    distTo[next] = distTo[prev] + 1;
+                }
+
+            }
+        }
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+        bfs();
     }
 }
 
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -9,16 +9,63 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s;
+    private boolean targetReached = false;
+    private Maze maze;
+    private int[] prev;
 
     public MazeCycles(Maze m) {
         super(m);
+        maze = m;
+        s = maze.xyTo1D(1, 1);
+        distTo[s] = 0;
+        edgeTo[s] = s;
+        prev = new int[maze.V()];
+        prev[s] = -1;
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        dfsCycle(s);
+    }
+
+    /** connect the cycle if detected one */
+    private void connectCycle(int v) {
+        for (int n : maze.adj(v)) {
+            if ((marked[n]) && (n != prev[v])) {
+                edgeTo[n] = v;
+                prev[n] = v;
+                int tmp = v;
+                while (tmp != n) {
+                    edgeTo[tmp] = prev[tmp];
+                    tmp = prev[tmp];
+                }
+                announce();
+                targetReached = true;
+                return;
+            }
+        }
     }
 
     // Helper methods go here
+    private void dfsCycle(int v) {
+        if (targetReached) {
+            return;
+        }
+
+        marked[v] = true;
+        announce();
+
+        connectCycle(v);
+
+        for (int n : maze.adj(v)) {
+            if (!marked[n]) {
+                marked[n] = true;
+                distTo[n] = distTo[v] + 1;
+                prev[n] = v;
+                dfsCycle(n);
+            }
+        }
+    }
 }
 
